40e16c2f70c509361e41817ac7697fee58654a75
Martin Jensen <mjmartinjensen@icloud.com>
2024-04-01 17:07:23 +0300

    add a button for the user to choose the grid size
    theodinproject project etchasketch #4 and #5

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+const buttonContainer = document.querySelector(".buttonContainer");
+
+const sizeButton = document.createElement("button");
+sizeButton.setAttribute("class","sizeButton");
+sizeButton.textContent = "Grid Size";
+buttonContainer.appendChild(sizeButton);
+
 const container = document.querySelector(".container");
 
-let j = 16;
-
-for (i = 0; i < j*j; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);
-}
+sizeButton.addEventListener("click", function (e){
+    let gridSize = prompt("Select Grid Size");
+    if (gridSize >= 1 && gridSize <=100) {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+          }
+        for (i = 0; i < gridSize*gridSize; i++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            container.appendChild(grid);
+            let gridWidth = (100/gridSize)
+            grid.style.flex = `${gridWidth}% 0 1`;
+        }
+    } else {
+        alert("Type a number between 1 and 100")
+    }
+})
 
 container.addEventListener('mouseover', function(event) {
     event.target.style.background = "black";
