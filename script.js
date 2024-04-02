cc9caa90ce03873e853897861c0d5c7b9bea411b
Martin Jensen <mjmartinjensen@icloud.com>
2024-04-02 13:00:32 +0300

    remove individual grid borders

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const buttonContainer = document.querySelector(".buttonContainer");
 
-const sizeButton = document.createElement("button");
-sizeButton.setAttribute("class","sizeButton");
-sizeButton.textContent = "Grid Size";
-buttonContainer.appendChild(sizeButton);
+const getGridSizeButton = document.createElement("button");
+getGridSizeButton.setAttribute("class","getGridSizeButton");
+getGridSizeButton.textContent = "Grid Size";
+buttonContainer.appendChild(getGridSizeButton);
 
 const container = document.querySelector(".container");
 
-sizeButton.addEventListener("click", function (e){
+getGridSizeButton.addEventListener("click", function(event){
     let gridSize = prompt("Select Grid Size");
-    if (gridSize >= 1 && gridSize <=100) {
-        while (container.firstChild) {
-            container.removeChild(container.lastChild);
-          }
-        for (i = 0; i < gridSize*gridSize; i++) {
-            const grid = document.createElement("div");
-            grid.classList.add("grid");
-            container.appendChild(grid);
-            let gridWidth = (100/gridSize)
-            grid.style.flex = `${gridWidth}% 0 1`;
+        if(gridSize >=1 && gridSize <= 100) {
+            while(container.firstChild){
+                container.removeChild(container.lastChild);
+            }
+            for (i = 0; i < gridSize*gridSize; i++){
+                const grid = document.createElement("div");
+                grid.classList.add("grid");
+                container.appendChild(grid);
+                let gridWidth = (100/gridSize);
+                grid.style.flex = `${gridWidth}% 0 1`;
+            }
+        } else {
+            alert("Type a number between 1 and 100")
         }
-    } else {
-        alert("Type a number between 1 and 100")
-    }
 })
 
 container.addEventListener('mouseover', function(event) {
