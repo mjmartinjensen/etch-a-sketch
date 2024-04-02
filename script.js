b4ba97cc46aa8bbf69ff821b5246ae6597402a6d
Martin Jensen <mjmartinjensen@icloud.com>
2024-04-02 21:55:47 +0300

    edit indentations spacing and simplifying code

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const buttonContainer = document.querySelector(".buttonContainer");
+const buttonContainer = document.querySelector('.buttonContainer');
 
-const getGridSizeButton = document.createElement("button");
-getGridSizeButton.setAttribute("class","getGridSizeButton");
-getGridSizeButton.textContent = "Grid Size";
-buttonContainer.appendChild(getGridSizeButton);
+const gridSizeButton = document.createElement('button');
+gridSizeButton.setAttribute('class','gridSizeButton');
+gridSizeButton.textContent = 'Grid Size';
+buttonContainer.appendChild(gridSizeButton);
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
-getGridSizeButton.addEventListener("click", function(event){
-    let gridSize = prompt("Select Grid Size");
-        if(gridSize >=1 && gridSize <= 100) {
-            while(container.firstChild){
-                container.removeChild(container.lastChild);
-            }
-            for (i = 0; i < gridSize*gridSize; i++){
-                const grid = document.createElement("div");
-                grid.classList.add("grid");
-                container.appendChild(grid);
-                let gridWidth = (100/gridSize);
-                grid.style.flex = `${gridWidth}% 0 1`;
-            }
-        } else {
-            alert("Type a number between 1 and 100")
-        }
+gridSizeButton.addEventListener('click', function(event) {
+    let gridSize = prompt('Select Grid Size');
+
+    if(gridSize < 1 && gridSize > 100) {
+        alert('Type a number between 1 and 100')
+
+        return;
+    }
+
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+
+    for (i = 0; i < gridSize * gridSize; i++) {
+        const grid = document.createElement('div');
+
+        grid.classList.add('grid');
+
+        container.appendChild(grid);
+
+        let gridWidth = 100 / gridSize;
+
+        grid.style.flex = `${gridWidth}% 0 1`;
+    }
+
 })
 
 container.addEventListener('mouseover', function(event) {
